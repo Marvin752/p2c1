013627a4e35f38015f0ee9891c029d1c3009951f
Alux <marvin75205@gmail.com>
2024-10-15 22:21:49 -0600

    Mira mamá, hice una aplicación

---
+++ app/src/main/java/gt/edu/umg/p2c1/MainActivity.java
@@ -0,0 +1,75 @@
+package gt.edu.umg.p2c1;
+
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import gt.edu.umg.p2c1.BaseDatos.DbHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSaludo, btnCrearDb, btnCrearRegistro;
+    TextView tvSaludo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        //Codigo nuevo
+        btnSaludo = findViewById(R.id.btnSaludo);
+        tvSaludo = findViewById(R.id.tvSaludo);
+        btnCrearDb = findViewById(R.id.btnCrearDb);
+        btnCrearRegistro = findViewById(R.id.btnCrearRegistro);
+
+        //Crear db
+
+        btnCrearDb.setOnClickListener(v ->{
+
+            //Crear base de datos
+
+            DbHelper dbHelper = new DbHelper(this);
+            dbHelper.getWritableDatabase();
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+            if(db != null)
+            {
+                Toast.makeText(this,"Creando base de datos", Toast.LENGTH_SHORT).show();
+                tvSaludo.setText("Creando base de datos");
+            }else{
+                Toast.makeText(this,"Error al crear base de datos", Toast.LENGTH_SHORT).show();
+                tvSaludo.setText("Error al crear base de datos");
+            }
+        });
+
+        //crear registro llama a la activity NuevoActivity FALTA CREAR Y REFERENCIAR EL BOTON
+        
+        btnCrearRegistro.setOnClickListener(v -> {
+            Toast.makeText(this, "Creando Registro", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, NuevoActivity.class);
+            startActivity(intent);
+
+        });
+
+        btnSaludo.setOnClickListener(v ->{
+            Toast.makeText(this,"Aviso Marvin", Toast.LENGTH_SHORT).show();
+            tvSaludo.setText("Hola Marvin");
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
